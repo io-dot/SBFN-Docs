55ee234683502a58680e167f869fa5d8e8082349
Ion Obreja <jo@da.md>
2023-09-04 09:34:03 +0300

    Add some content

---
+++ src/content/docs/general-styles/font-size-headings.md
@@ -0,0 +1,15 @@
+---
+title: Font Size & Headings
+---
+
+| tag/class             | Size          |
+|-----------------------|:-------------:|
+| <h1>H1: Heading1</h1> | <h1>64px</h1> |
+| <h2>H2: Heading2</h2> | <h2>40px</h2> |
+| <h3>H3: Heading3</h3> | <h3>32px</h3> |
+| <h4>H4: Heading4</h4> | <h4>24px</h4> |
+| <h5>H5: Heading5</h5> | <h5>18px</h5> |
+| <h6>H6: Heading6</h6> | <h6>16px</h6> |
+| Body text             | 16px          |
+| Small text            | 14px          |
+| Input label           | 10px          |
--- src/site_config.ts
+++ src/site_config.ts
@@ -3,12 +3,22 @@ export const SITE_TITLE = "SBFN Docs"
 export const SITE_DESCRIPTION = "A documentation template for SBFN"
 export const SITE_URL = "https://github.com/io-dot"
 export const SITE_DEFAULT_OG_IMAGE = "/assets/og-image.png"
+export const GIT_REPO = "/SBFN-Docs"
 
 // Docs Sidebar temp
 // The path should match the folder name in src/content/docs/
 export const SIDEBAR_ITEMS = {
-  "Get Started": [
-    "/SBFN-Docs/docs/get-started/introduction",
+  "General styles": [
+    `${GIT_REPO}/docs/general-styles/colors`,
+    `${GIT_REPO}/docs/general-styles/font-size-headings`,
+    `${GIT_REPO}/docs/general-styles/buttons`,
+  ],
+  "General Components": [
+    `${GIT_REPO}/docs/get-started/introduction`,
+    "/SBFN-Docs/docs/get-started/installation",
+  ],
+  "SBFN Components": [
+    `${GIT_REPO}/docs/get-started/introduction`,
     "/SBFN-Docs/docs/get-started/installation",
   ],
   "Tutorials": [
